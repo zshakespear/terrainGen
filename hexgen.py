6697835301d8bc3231015c1349aac84761f1b692
Zac Shakespear <zshakespear@gmail.com>
2022-03-24 11:05:01 -0600

    Began implementation

--- hexgen.py
+++ hexgen.py
@@ -1,2 +1,25 @@
 import hex
 
+path = [1 for x in range(0,20)]
+
+seed = input('Input the seed biome: \n0: Desert\n1: Hill\n2: Mountain\n3: Plain\n4: Wetland\n5: Forest\n')
+while seed != '0' and seed != '1' and seed != '2' and seed != '3' and seed != '4' and seed != '5':
+    seed = input('Invalid input.\nInput the seed biome: \n0: Desert\n1: Hill\n2: Mountain\n3: Plain\n4: Wetland\n5: Forest\n')
+
+seed = int(seed)
+
+if seed == 0:
+    seedb = 'desert'
+if seed == 1:
+    seedb = 'hill'
+if seed == 2:
+    seedb = 'mountain'
+if seed == 3:
+    seedb = 'plain'
+if seed == 4:
+    seedb = 'wetland'
+if seed == 5:
+    seedb = 'forest'
+    
+currenth = hex.Hex(seedb)
+
