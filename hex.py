53fa573afd27fee358a394cdae1b9fe9589e3abc
Zac Shakespear <zshakespear@gmail.com>
2022-03-24 10:04:17 -0600

    implemented neighborhood generation

--- hex.py
+++ hex.py
@@ -1,8 +1,12 @@
 import random
 
 class Hex:
-    #Will want to prohibit the creation of hexes without standard biome types
+
     def __init__(self, b):
+        if b != 'forest' and b!= 'plain' and b != 'desert' and b != 'mountain' and b != 'hill' and b != 'wetland':
+                print('invalid initializer')
+                return
+        
         self.biome = b
         self.neigh1 = None
         self.neigh2 = None
@@ -46,27 +50,128 @@ class Hex:
     def genNeighbors(self):
         if self.neigh1 == None:
             print("Initializing neighbor 1")
+            seedb = self.helperBiome()
+            self.setNeigh1(Hex(seedb))
+            
         if self.neigh2 == None:
             print("Initializing neighbor 2")
+            seedb = self.helperBiome()
+            self.setNeigh2(Hex(seedb))
+            
         if self.neigh3 == None:
             print("Initializing neighbor 3")
+            seedb = self.helperBiome()
+            self.setNeigh3(Hex(seedb))
+            
         if self.neigh4 == None:
             print("Initializing neighbor 4")
+            seedb = self.helperBiome()
+            self.setNeigh4(Hex(seedb))
+            
         if self.neigh5 == None:
             print("Initializing neighbor 5")
+            seedb = self.helperBiome()
+            self.setNeigh5(Hex(seedb))
+            
         if self.neigh6 == None:
             print("Initializing neighbor 6")
+            seedb = self.helperBiome()
+            self.setNeigh6(Hex(seedb))
+        self.helperLink()
             
+    def helperLink(self):
+        self.neigh1.setNeigh3(self.neigh2)
+        self.neigh1.setNeigh5(self.neigh6)
+        self.neigh1.setNeigh4(self)
+        
+        self.neigh2.setNeigh4(self.neigh3)
+        self.neigh2.setNeigh6(self.neigh1)
+        self.neigh2.setNeigh5(self)
+        
+        self.neigh3.setNeigh1(self.neigh2)
+        self.neigh3.setNeigh5(self.neigh4)
+        self.neigh3.setNeigh6(self)
+        
+        self.neigh4.setNeigh2(self.neigh3)
+        self.neigh4.setNeigh6(self.neigh5)
+        self.neigh4.setNeigh1(self)
+        
+        self.neigh5.setNeigh1(self.neigh6)
+        self.neigh5.setNeigh3(self.neigh4)
+        self.neigh5.setNeigh2(self)
+        
+        self.neigh6.setNeigh2(self.neigh1)
+        self.neigh6.setNeigh4(self.neigh5)
+        self.neigh6.setNeigh3(self)
+    
     def helperBiome(self):
         if self.biome == "forest" :
-            print("This is a forest")
+            #print("This is a forest")
+            seed = random.randint(0, 3)
+            if seed == 0:
+                seedb = 'forest'
+            if seed == 1:
+                seedb = 'hill'
+            if seed == 2:
+                seedb = 'plain'
+            if seed == 3:
+                seedb = 'wetland'
+            return seedb
+                
         if self.biome == "desert" :
-            print("This is a desert")
+            #print("This is a desert")
+            seed = random.randint(0,2)
+            if seed == 0:
+                seedb = 'desert'
+            if seed == 1:
+                seedb = 'plain'
+            if seed == 2:
+                seedb = 'hill'
+            return seedb
+        
         if self.biome == "hill" :
-            print("This is a hill")
+            #print("This is a hill")
+            seed = random.randint(0,8)
+            if seed == 0:
+                seedb = 'mountain'
+            if seed == 1 or seed == 2:
+                seedb = 'hill'
+            if seed == 3 or seed == 4:
+                seedb = 'plain'
+            if seed == 5 or seed == 6:
+                seedb = 'forest'
+            if seed == 7 or seed == 8:
+                seedb = 'wetland'
+            return seedb
+        
         if self.biome == "mountain" :
-            print("This is a mountain")
+            #print("This is a mountain")
+            seed = random.randint(0,5)
+            if seed == 0:
+                seedb = 'mountain'
+            else:
+                seedb = 'hill'
+            return seedb
+
         if self.biome == "plain" :
-            print("This is a plain")
+            #print("This is a plain")
+            seed = random.randint(0, 4)
+            if seed == 0:
+                seedb = 'plain'
+            if seed == 1:
+                seedb = 'wetland'
+            if seed == 2:
+                seedb = 'hill'
+            if seed == 3:
+                seedb = 'forest'
+            if seed == 4:
+                seedb = 'desert'
+            return seedb
+        
         if self.biome == "wetland" :
-            print("This is a wetland")
+            #print("This is a wetland")
+            seed = random.randint(0,1)
+            if seed == 0:
+                seedb = 'wetland'
+            if seed ==1:
+                seedb = 'plain'
